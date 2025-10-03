7e935a42c71ca10fa475f9d8bb4034931d7bb707
kaiqueaq <kaiquesilvaaquino2@gmail.com>
2025-10-02 21:47:34 -0300

    fix

--- js/api.js
+++ js/api.js
@@ -1,5 +1,10 @@
 const API_URL = "http://localhost:8080";
 
+// Suppress console messages in production to avoid clutter
+if (window.location.hostname !== 'localhost') {
+  console.log = console.warn = console.error = console.info = console.debug = () => {};
+}
+
 async function apiPost(path, body) {
   const response = await fetch(API_URL + path, {
     method: "POST",
